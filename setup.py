8ba683f59f0e422fc5442aa684afd3d73ef67362
Nat1405 <nathanc1945@gmail.com>
2017-04-14 03:59:34 -0700

    Changed directory structure and completed first draft of setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,8 @@
 from distutils.core import setup
 setup(name='nifty',
               version='0.1.0',
-                    py_modules=['nifsDefs.py','nigsFluxCalib.py','nifsMerge.py','nifsReduce.py','nifsScience.py','nifsSort.py','nifsTelluric.py'],
+                    py_modules=['src.nifsDefs','src.nifsFluxCalib','src.nifsMerge','src.nifsReduce','src.nifsScience','src.nifsSort','src.nifsTelluric'],
+                    scripts=['src/nifty'],
+                    data_files=[('', ['src/vega_ext.fits']),
+                                ('', ['src/starstemp.txt'])]
                           )
