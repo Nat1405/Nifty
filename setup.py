f2b667a9ca1ad365f5e27c915d9ae0fff455bb3f
Nat1405 <nathanc1945@gmail.com>
2017-04-14 03:28:19 -0700

    First attempt packaging as a Python package.

---
+++ setup.py
@@ -0,0 +1,5 @@
+from distutils.core import setup
+setup(name='nifty',
+              version='0.1.0',
+                    py_modules=['nifsDefs.py','nigsFluxCalib.py','nifsMerge.py','nifsReduce.py','nifsScience.py','nifsSort.py','nifsTelluric.py'],
+                          )
